9d21d66c9e3606d0ff9d6be03603c5b77a09a18e
dextercarlmiller <36194823+dextercarlmiller@users.noreply.github.com>
2019-11-07 12:40:58 -0500

    It's prettier now

--- bstimp.cpp
+++ bstimp.cpp
@@ -1,12 +1,19 @@
 #include "BST.h"
 
-bst::bst()
-{
-root = NULL;
+bst::bst(){
+	root = NULL;
 }
-
-void bst::insert(node*n, int data)
-{
+void bst::add(int data){
+	insert (root,data);
+}
+node* bst::createNode(int data){
+	node* Newnode = new node;
+	Newnode->data = data;
+	Newnode->left = NULL;
+	Newnode->right = NULL;
+	return Newnode;
+}
+void bst::insert(node*n, int data){
 	// if nothing, create
 	if (root == NULL)
 	{
@@ -37,22 +44,7 @@ void bst::insert(node*n, int data)
 		}
 	}
 }
-
-node* bst::createNode(int data)
-{
-node* Newnode = new node;
-Newnode->data = data;
-Newnode->left = NULL;
-Newnode->right = NULL;
-return Newnode;
-}
-
-void bst::add(int data)
-{
-	insert (root,data);
-}
-int bst::maxDepth(node* node)  
-{  
+int bst::maxDepth(node* node){  
     if (node == NULL)  
         return 0;  
     else
@@ -72,53 +64,45 @@ void bst::maxDepthRun(){
 	cout << "The max Depth is:" << depth << endl;
 }
 void bst::inorderRecursion(node*n){
-if (n != NULL){
-	inorderRecursion(n->left);
-	cout << n->data << endl;
-	inorderRecursion(n->right);
-	}
+	if (n != NULL){
+		inorderRecursion(n->left);
+		cout << n->data << endl;
+		inorderRecursion(n->right);
+		}
 }
 void bst::preorderRecursion(node*n){
-if(n != NULL){
-	cout << n->data << endl;
-	preorderRecursion(n->left);
-	preorderRecursion(n->right);
+	if(n != NULL){
+		cout << n->data << endl;
+		preorderRecursion(n->left);
+		preorderRecursion(n->right);
+	}
 }
+void bst::postorderRecursion(node*n){
+	if (n != NULL){
+		postorderRecursion(n->left);
+		postorderRecursion(n->right);
+		cout << n->data << endl;
+	}
 }
-void bst::postorderRecursion(node*n)
-{
-if (n != NULL){
-	postorderRecursion(n->left);
-	postorderRecursion(n->right);
-	cout << n->data << endl;
+void bst::postorderprint(){
+	postorderRecursion(root);
 }
+void bst::preorderprint(){
+	preorderRecursion(root);
 }
+void bst::inorderprint(){
+	inorderRecursion(root);
+}
+
 //Deconstructor
-void bst::clear(node*n)
-{
-if(n->left != NULL)
-	clear(n->left);	
-else if (n->right != NULL)
-	clear(n->right);
-delete n;
+void bst::clear(node*n){
+	if(n->left != NULL)
+		clear(n->left);	
+	else if (n->right != NULL)
+		clear(n->right);
+	delete n;
 }
-void bst::destroy()
-{
-clear(root);
-}
-
-void bst::postorderprint()
-{
-postorderRecursion(root);
-}
-
-void bst::preorderprint()
-{
-preorderRecursion(root);
-}
-
-void bst::inorderprint()
-{
-inorderRecursion(root);
+void bst::destroy(){
+	clear(root);
 }
 
